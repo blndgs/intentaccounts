70db08b5bdad2339d0d39c1e57ab2532282054e3
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2024-01-19 21:44:52 +0200

    feat: enhance deployment script, vars for Mumbai

--- script/deploySimpleAccountFactory.s.sol
+++ script/deploySimpleAccountFactory.s.sol
@@ -8,30 +8,54 @@ contract deploySimpleAccountFactory is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 startGas = gasleft();
 
-        console.log("Deploying from address:", msg.sender);
-        console.log("Network ID:", block.chainid);
-        console.log("Balance of Deployer:", address(msg.sender).balance);
+        bool dryRun = vm.envBool("DRY_RUN");
+        console2.log("Dry run:", dryRun);
+        if (dryRun) {
+            uint256 fork = vm.createFork(vm.envString("MUMBAI_RPC_URL")); // Fork the Mumbai network
+            vm.selectFork(fork);
+        }
+
+        // Setup signer
+        string memory privateKeyString = vm.envString("MUMBAI_PRIVATE_KEY");
+        bytes memory privateKeyBytes = bytes(privateKeyString);
+        require(privateKeyBytes.length == 32, "Invalid private key length");
+
+        uint256 privateKey = vm.parseUint(privateKeyString);
+        address signer = vm.addr(privateKey);
+        vm.startBroadcast(signer);
+
+        console2.log("Deploying from address:", msg.sender);
+        console2.log("Network ID:", block.chainid);
+        console2.log("Balance of Deployer:", address(msg.sender).balance);
+        console2.log("Owner of SimpleAccount", signer);
 
         // Define entry point address and owner address
         address entryPointAddress = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
-        address eoaOwner = 0xa4BFe126D3aD137F972695dDdb1780a29065e556;
 
         // Deploy the SimpleAccountFactory with the EntryPoint
         SimpleAccountFactory factory = new SimpleAccountFactory(IEntryPoint(entryPointAddress));
-        console.log("SimpleAccountFactory deployed at:", address(factory));
+        console2.log("SimpleAccountFactory deployed at:", address(factory));
+        uint256 endGas = gasleft();
+        console2.log("Gas used for Factory deployment: ", startGas - endGas);
+        startGas = endGas;
 
         // Create a unique salt for the account creation
-        uint256 salt = 0;
+        uint256 salt = vm.envUint("MUMBAI_SALT");
+        console2.log("Salt:", salt);
 
         // Use the factory to create a new SimpleAccount instance
-        SimpleAccount account = factory.createAccount(eoaOwner, salt);
-        console.log("SimpleAccount created by factory at:", address(account));
+        SimpleAccount account = factory.createAccount(signer, salt);
+        endGas = gasleft();
+        console2.log("Gas used for account deployment: ", startGas - endGas);
+        startGas = endGas;
 
         // Optionally, verify the created account's address matches the expected counterfactual address
-        address expectedAddress = factory.getAddress(eoaOwner, salt);
-        console.log("Expected counterfactual address of created SimpleAccount:", expectedAddress);
+        address expectedAddress = factory.getAddress(signer, salt);
+        console2.log("Counterfactual address of SimpleAccount:", expectedAddress);
+        console2.log("SimpleAccount created by factory at:", address(account));
+        console2.log("Gas used for account getAddress(): ", startGas - endGas);
 
         vm.stopBroadcast();
     }
