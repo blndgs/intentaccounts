d90286aa381200d5cd67d08df3d035a0a51ba1cc
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2024-03-06 14:45:38 +0200

    feat: Add script file and move deploy scripts to own directory

--- script/deploy/deploy-mainnet-cmd.sh
+++ script/deploy/deploy-mainnet-cmd.sh
@@ -1,1 +1,1 @@
-forge script script/deploy/SimpleAccountFactory.s.sol --broadcast --rpc-url https://mainnet.gateway.tenderly.co/30Ficv49C4MUExEKiJgC1z -vvvvv --chain-id 1 --verify --watch
+forge script DeploySimpleAccountFactory --broadcast --rpc-url https://mainnet.gateway.tenderly.co/30Ficv49C4MUExEKiJgC1z -vvvvv --chain-id 1 --verify --watch
