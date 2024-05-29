615615687bb739f0c69fe26013db726be3284a5f
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2024-05-30 00:45:34 +0300

    fix: Remove unecessary caller check

--- src/KernelIntentExecutor.sol
+++ src/KernelIntentExecutor.sol
@@ -6,19 +6,12 @@ import {IKernel} from "../lib/kernel/src/interfaces/IKernel.sol";
 
 contract KernelIntentExecutor {
     address private constant ENTRYPOINT_V06 = 0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789;
+
     event ExecutorDoNothing();
 
     function doNothing() external {
         // do nothing
         emit ExecutorDoNothing();
-    }
-
-    // Modifier to check if the function is called by the entry point, the contract itself or the owner
-    modifier onlyFromEntryPointOrSelf() {
-        if (msg.sender != ENTRYPOINT_V06 && msg.sender != address(this)) {
-            revert IKernel.NotAuthorizedCaller();
-        }
-        _;
     }
 
     function _call(address target, uint256 value, bytes memory data) internal {
@@ -33,14 +26,14 @@ contract KernelIntentExecutor {
     /**
      * execute a transaction (called directly from owner, or by entryPoint)
      */
-    function execute(address dest, uint256 value, bytes calldata func) external onlyFromEntryPointOrSelf{
+    function execute(address dest, uint256 value, bytes calldata func) external {
         _call(dest, value, func);
     }
 
     /**
      * execute a sequence of transactions
      */
-    function executeBatch(address[] calldata dest, bytes[] calldata func) external onlyFromEntryPointOrSelf {
+    function executeBatch(address[] calldata dest, bytes[] calldata func) external {
         require(dest.length == func.length, "wrong array lengths");
         for (uint256 i = 0; i < dest.length; i++) {
             _call(dest[i], 0, func[i]);
@@ -50,11 +43,10 @@ contract KernelIntentExecutor {
     /**
      * execute a sequence of EVM calldata with Ether transfers.
      */
-    function execValueBatch(uint256[] calldata values, address[] calldata dest, bytes[] calldata func) external onlyFromEntryPointOrSelf {
+    function execValueBatch(uint256[] calldata values, address[] calldata dest, bytes[] calldata func) external {
         require(dest.length == func.length, "wrong array lengths");
         for (uint256 i = 0; i < dest.length; i++) {
             _call(dest[i], values[i], func[i]);
         }
     }
-
 }
