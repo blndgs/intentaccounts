53b027b0fbbbb424f0dd68182018287f2f336bd4
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2024-07-31 20:33:42 +0300

    feat: passing unit test

--- src/IntentSimpleAccount.sol
+++ src/IntentSimpleAccount.sol
@@ -90,58 +90,34 @@ contract IntentSimpleAccount is SimpleAccount {
         bytes callData;
     }
 
-    function extractDestUserOp(UserOperation calldata combinedOp) external pure returns (UserOperation memory) {
-        console2.log("Entered extractDestUserOp");
-        console2.log("combinedOp.callData length:", combinedOp.callData.length);
-
+    function extractDestUserOp(UserOperation calldata combinedOp) public returns (UserOperation memory) {
         (uint256 sourceCallDataLength, bytes memory packedDestOpData) = extractPackedData(combinedOp.callData);
-        console2.log("sourceCallDataLength:", sourceCallDataLength);
 
         PackedUserOp memory packedDestOp = decodePackedUserOp(packedDestOpData);
 
-        return unpackUserOp(packedDestOp);
+        UserOperation memory result = unpackUserOp(packedDestOp);
+
+        return result;
     }
 
     function extractPackedData(bytes calldata callData) internal pure returns (uint256, bytes memory) {
         require(callData.length >= 32, "Invalid callData length");
 
         uint256 sourceCallDataLength = uint256(bytes32(callData[:32]));
-        console2.log("sourceCallDataLength:", sourceCallDataLength);
 
         require(callData.length > 32 + sourceCallDataLength, "Invalid callData format");
 
         bytes memory packedDestOpData = callData[32 + sourceCallDataLength:];
-        console2.log("packedDestOpData length:", packedDestOpData.length);
 
         require(packedDestOpData.length > 0, "No packed destination UserOp found");
-
-        console2.log("packedDestOpData:");
-        console2.logBytes(packedDestOpData);
 
         return (sourceCallDataLength, packedDestOpData);
     }
 
     function decodePackedUserOp(bytes memory packedDestOpData) internal pure returns (PackedUserOp memory) {
-        console2.log("Attempting to decode PackedUserOp");
-
         PackedUserOp memory packedDestOp = abi.decode(packedDestOpData, (PackedUserOp));
 
-        console2.log("Decoding successful");
-        logPackedUserOp(packedDestOp);
-
         return packedDestOp;
-    }
-
-    function logPackedUserOp(PackedUserOp memory packedDestOp) internal pure {
-        console2.log("Decoded packedDestOp:");
-        console2.log("  sender:", packedDestOp.sender);
-        console2.log("  nonce:", packedDestOp.nonce);
-        console2.log("  callGasLimit:", packedDestOp.callGasLimit);
-        console2.log("  verificationGasLimit:", packedDestOp.verificationGasLimit);
-        console2.log("  preVerificationGas:", packedDestOp.preVerificationGas);
-        console2.log("  maxFeePerGas:", packedDestOp.maxFeePerGas);
-        console2.log("  maxPriorityFeePerGas:", packedDestOp.maxPriorityFeePerGas);
-        console2.log("  callData length:", packedDestOp.callData.length);
     }
 
     function unpackUserOp(PackedUserOp memory packedOp) internal pure returns (UserOperation memory) {
