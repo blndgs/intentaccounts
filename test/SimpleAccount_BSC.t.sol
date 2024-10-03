3e0fa2c0888d2cb880c190b38ecc06a368bee4e4
Mario Karagiorgas <2580304+blewater@users.noreply.github.com>
2024-10-04 02:57:32 +0300

    fix: Remove commented import statement

--- test/SimpleAccount_BSC.t.sol
+++ test/SimpleAccount_BSC.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
-// import "forge-std/Test.sol";
 import "../src/IntentSimpleAccount.sol";
 import "../src/IntentSimpleAccountFactory.sol";
 import "@account-abstraction/interfaces/IEntryPoint.sol";
